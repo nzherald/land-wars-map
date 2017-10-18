54381ba31616fac5d13c062c8bc563cb6c5f1793
macteariki <macteariki@gmail.com>
2017-10-19 10:02:05 +1300

    Plot battle sites

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 var data = [
   {
+    "campaign": "Wairau",
+    "year": "1843"
+  },
+  {
     "campaign": "Northern Wars",
     "year": "1845"
   },
   {
-    "campaign": "Wairau",
-    "year": "1843"
-  }
+    "campaign": "Hutt Valley",
+    "year": "1846"
+  },
+  {
+    "campaign": "Taranaki",
+    "year": "1860"
+  },
+  {
+    "campaign": "Waikato",
+    "year": "1863-64"
+  },
+  {
+    "campaign": "East Coast",
+    "year": "1865-67"
+  },
+  {
+    "campaign": "South Taranaki",
+    "year": "1865-69"
+  },
+  {
+    "campaign": "Te Kooti",
+    "year": "1868-72"
+  },
 ];
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibnpoZXJhbGQiLCJhIjoiSVBPNHM0cyJ9.PDW_j3xU8w-wTnKCpnshPg';
@@ -18,3 +42,14 @@ var map = new mapboxgl.Map({
   minZoom: 4.8,
   maxZoom: 16
 });
+
+var timeline_container = document.getElementById('wrapper');
+
+var campaigns = data.map((d,i) => {
+  return d;
+});
+
+function clicked() {
+  console.log(data[0].campaign)
+
+}
