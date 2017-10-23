f9d1504707b1faf4b1856814365fd03540da7da2
Chris Knox <chris.knox@nzherald.co.nz>
2017-10-24 11:29:02 +1300

    webpackify

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './styles.scss'
+
 var data = [
   {
     "campaign": "Wairau",
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.[hash].js'
+  },
+    resolve: {
+        modules: [path.resolve(__dirname, './src'), 'node_modules']
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './assets'),
+    },
+module: {
+        rules: [
+            {
+                test: /\.md$/,
+                include: [/src/],
+                loader: 'markdown-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!postcss-loader!sass-loader"
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                loader: 'url-loader'
+            },
+            {
+              test: /\.yaml$/,
+              loader: 'yml-loader'
+            },
+          {
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, "src"),
+      ],
+      test: /\.(js|jsx|es6)$/,
+      exclude: /(node_modules|bower_components)/,
+      query: {
+        plugins: ['transform-runtime'],
+        presets: ['env'],
+      }
+    }
+        ]
+    },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      // favicon: 'src/images/favicon.ico',
+      title: 'NZH Insights'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'assets/images', to: 'images' }
+    ])
+  ]
+};
+
+module.exports = config;
